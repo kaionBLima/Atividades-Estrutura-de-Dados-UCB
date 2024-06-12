e82b45bd0b48079009706c5957ec123fa5088cae
kaionBLima <kaionbrandaolima@gmail.com>
2024-06-12 19:33:23 -0300

    ultimo commit

--- Trabalho_Wakanda/cidades.c
+++ Trabalho_Wakanda/cidades.c
@@ -31,7 +31,7 @@ Estrada *getEstrada(const char *nomeArquivo) {
 
     fscanf(arquivo, "%d", &(estrada->T));
     fscanf(arquivo, "%d", &(estrada->N));
-    fgetc(arquivo); // Consome o caractere de nova linha após o número de cidades
+    fgetc(arquivo);
 
     estrada->C = (Cidade *)malloc(estrada->N * sizeof(Cidade));
     if (!estrada->C) {
@@ -47,7 +47,7 @@ Estrada *getEstrada(const char *nomeArquivo) {
         sscanf(linha, "%d %[^\n]", &(estrada->C[i].Posicao), estrada->C[i].Nome);
     }
 
-    ordenarCidades(estrada->C, estrada->N);  // utilizado para ordenar as cidades
+    ordenarCidades(estrada->C, estrada->N);
 
     fclose(arquivo);
     return estrada;
@@ -57,7 +57,7 @@ double calcularMenorVizinhanca(const char *nomeArquivo) {
     Estrada *estrada = getEstrada(nomeArquivo);
     if (!estrada) return -1;
 
-    double menorVizinhanca = estrada->T; // Inicializa com o comprimento total da estrada
+    double menorVizinhanca = estrada->T;
     for (int i = 0; i < estrada->N - 1; i++) {
         double vizinhancaAtual = (estrada->C[i + 1].Posicao - estrada->C[i].Posicao) / 2.0;
         if (vizinhancaAtual < menorVizinhanca) {
@@ -74,7 +74,7 @@ char *cidadeMenorVizinhanca(const char *nomeArquivo) {
     Estrada *estrada = getEstrada(nomeArquivo);
     if (!estrada) return NULL;
 
-    double menorVizinhanca = estrada->T; // Inicializa com o comprimento total da estrada
+    double menorVizinhanca = estrada->T;
     char *cidadeMenor = (char *)malloc(256 * sizeof(char));
     if (!cidadeMenor) {
         perror("Erro ao alocar memória para cidadeMenor");
@@ -91,7 +91,7 @@ char *cidadeMenorVizinhanca(const char *nomeArquivo) {
         }
     }
 
-    // Verificar último trecho da estrada
+
     vizinhancaAtual = (estrada->T - estrada->C[estrada->N - 1].Posicao) / 2.0;
     if (vizinhancaAtual < menorVizinhanca) {
         menorVizinhanca = vizinhancaAtual;
